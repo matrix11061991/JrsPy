c8b45c1997594b62a28a59767b6745ff88f2e6f7
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2023-01-29 16:59:32 +0200

    Update setup.py

--- YnuTil/setup.py
+++ YnuTil/setup.py
@@ -1,1 +1,28 @@
 
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='YnuTil',
+    version='0.1.0',
+    author='Your Name',
+    author_email='anpproba@gmail.com',
+    description='A simple python project',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/matrix11061991/JrsPy',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+        'numpy',
+        'pandas'
+    ],
+)
